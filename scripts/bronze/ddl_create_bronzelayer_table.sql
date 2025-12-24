009aed2122065ed3d15eda9949ee8523bf0918da
Ratanak Buth <157795827+ratanakbuth@users.noreply.github.com>
2025-12-24 13:53:36 +0100

    Standardize column names to lowercase in SQL tables

--- scripts/bronze/ddl_create_bronzelayer_table.sql
+++ scripts/bronze/ddl_create_bronzelayer_table.sql
@@ -49,23 +49,23 @@ Create table bronze.crm_sales_details (
 IF OBJECT_ID ('bronze.erp_cust_az12','U') IS NOT NULL
 	DROP TABLE bronze.erp_cust_az12;
 Create table bronze.erp_cust_az12 (
-	CID NVARCHAR(50),
-	BDATE DATE,
-	GEN NVARCHAR(50)
+	cid NVARCHAR(50),
+	bdate DATE,
+	gen NVARCHAR(50)
 );
 
 IF OBJECT_ID ('bronze.erp_loc_a101','U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
 Create table bronze.erp_loc_a101 (
-	CID NVARCHAR(50),
-	CNTRY NVARCHAR(50)
+	cid NVARCHAR(50),
+	cntry NVARCHAR(50)
 );
 
 IF OBJECT_ID ('bronze.erp_px_cat_g1v2','U') IS NOT NULL
 	DROP TABLE bronze.erp_px_cat_g1v2;
 Create table bronze.erp_px_cat_g1v2 (
-	ID NVARCHAR(50),
-	CAT NVARCHAR(50),
-	SUBCAT NVARCHAR(50),
-	MAINTENANCE NVARCHAR(50)
+	id NVARCHAR(50),
+	cat NVARCHAR(50),
+	subcat NVARCHAR(50),
+	maintenance NVARCHAR(50)
 );
